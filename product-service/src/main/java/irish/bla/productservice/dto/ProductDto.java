f792ad5040ebfa198ab5c788c4ce81034dc6aa50
weoamathior <weoamathior@gmail.com>
2024-01-13 06:22:02 -0800

    basic product setup

---
+++ product-service/src/main/java/irish/bla/productservice/dto/ProductDto.java
@@ -0,0 +1,11 @@
+package irish.bla.productservice.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+    private String id;
+    private String description;
+    private Integer price;
+
+}
---
+++ product-service/src/main/java/irish/bla/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package irish.bla.productservice.repository;
+
+import irish.bla.productservice.entity.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product,String> {
+}
