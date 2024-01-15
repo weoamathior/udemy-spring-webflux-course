da577484795cb4bb918552f996e0893fe61530de
weoamathior <weoamathior@gmail.com>
2024-01-15 09:35:06 -0800

    user service

---
+++ user-service/src/main/java/irish/bla/userservice/service/UserServivce.java
@@ -0,0 +1,47 @@
+package irish.bla.userservice.service;
+
+import irish.bla.userservice.dto.UserDto;
+import irish.bla.userservice.repository.UserRepository;
+import irish.bla.userservice.util.EntityDtoUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserServivce {
+
+    private final UserRepository userRepository;
+
+    public Flux<UserDto> all() {
+        return userRepository.findAll()
+                .map(EntityDtoUtil::toDto);
+    }
+
+    public Mono<UserDto> getUserById(Integer userId) {
+        return userRepository.findById(userId).map(EntityDtoUtil::toDto);
+    }
+
+    public Mono<UserDto> createUser(Mono<UserDto> userDto) {
+        return userDto.map(EntityDtoUtil::toEntity)
+                .flatMap(userRepository::save)
+                .map(EntityDtoUtil::toDto);
+    }
+
+    public Mono<UserDto> updateUser(Integer id, Mono<UserDto> userDto) {
+        return userRepository.findById(id)
+                .flatMap(u -> userDto.map(EntityDtoUtil::toEntity)
+                        .doOnNext(e -> e.setId(id)))
+                .flatMap(userRepository::save)
+                .map(EntityDtoUtil::toDto);
+    }
+
+    public Mono<Void> deleteUser(Integer id) {
+        return userRepository.deleteById(id);
+
+    }
+
+}
