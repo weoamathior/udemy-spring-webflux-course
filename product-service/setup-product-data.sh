f138fc99312752ef06089cb3f37d4ddeae25bd18
weoamathior <weoamathior@gmail.com>
2024-01-15 08:17:39 -0800

    add some products script

---
+++ product-service/setup-product-data.sh
@@ -0,0 +1,4 @@
+curl -XPOST -H "Content-type: application/json" -d '{"description":"shebang","price":40}' localhost:8091/product
+curl -XPOST -H "Content-type: application/json" -d '{"description":"foo","price":10}' localhost:8091/product
+curl -XPOST -H "Content-type: application/json" -d '{"description":"bar","price":20}' localhost:8091/product
+curl -XPOST -H "Content-type: application/json" -d '{"description":"hokum","price":30}' localhost:8091/product
