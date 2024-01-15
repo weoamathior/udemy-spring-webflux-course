96e5329d3dd1b79c904a12532d29a7e58c0a426c
weoamathior <weoamathior@gmail.com>
2024-01-15 09:54:40 -0800

    update user balance query with modifying annotation

--- user-service/src/main/java/irish/bla/userservice/repository/UserRepository.java
+++ user-service/src/main/java/irish/bla/userservice/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package irish.bla.userservice.repository;
 
 import irish.bla.userservice.entity.User;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User,Integer> {
+
+    @Modifying
+    @Query("update users set balance = balance - :amount where id = :userId and balance >= :amount")
+    Mono<Boolean> updateUserBalance(Integer userId, Integer amount);
 }
