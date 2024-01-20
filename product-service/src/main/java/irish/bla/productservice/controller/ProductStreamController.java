2a85dcc8fa21fa1107a1ce30d9e0a61b32d62688
weoamathior <weoamathior@gmail.com>
2024-01-20 09:36:22 -0800

    product streaming controller

---
+++ product-service/src/main/java/irish/bla/productservice/controller/ProductStreamController.java
@@ -0,0 +1,21 @@
+package irish.bla.productservice.controller;
+
+import irish.bla.productservice.dto.ProductDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("product")
+@RequiredArgsConstructor
+public class ProductStreamController {
+    private final Flux<ProductDto> flux;
+
+    @GetMapping(value = "stream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductDto> getProductUpdates() {
+        return flux;
+    }
+}
