68a8e6cc37227ddaaf58a19686bbacebc049871e
weoamathior <weoamathior@gmail.com>
2024-01-20 10:02:31 -0800

    sse events frontend demo

--- product-service/src/main/java/irish/bla/productservice/controller/ProductStreamController.java
+++ product-service/src/main/java/irish/bla/productservice/controller/ProductStreamController.java
@@ -4,6 +4,7 @@ import irish.bla.productservice.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -17,5 +18,11 @@ public class ProductStreamController {
     @GetMapping(value = "stream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ProductDto> getProductUpdates() {
         return flux;
+    }
+
+    @GetMapping(value = "stream/{maxPrice}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductDto> getProductUpdates(@PathVariable Integer maxPrice) {
+        return flux
+                .filter(dto -> dto.getPrice() <= maxPrice);
     }
 }
